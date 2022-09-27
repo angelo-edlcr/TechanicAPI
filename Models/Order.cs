d7df9c7b0d5f16cd41b1f21f346e36ec6d07e0ef
Angelo De La Cruz <99463079+angelo-edlcr@users.noreply.github.com>
2022-09-27 08:05:04 -0400

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechanicAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int productId { get; set; }
+        public int quantity {get; set; }
+
+        public double total { get; set; }
+    }
+}
---
+++ Models/TechanicContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using TechanicAPI.Models;
+
+namespace TechanicAPI
+{
+    public partial class TechanicContext : DbContext
+    {
+        public TechanicContext()
+        {
+        }
+
+        public TechanicContext(DbContextOptions<TechanicContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=ec2-3-229-165-146.compute-1.amazonaws.com;port=5432;Username=qkczcibuzywsks;Password=3322e7156da0101f351c4a40ce3660f40f6de1c3365cbe5b7df4193534c6bc6a;Database=d87b3u4re3mhr7");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.CustomerId).HasColumnName("customer_id");
+
+                entity.Property(e => e.productId)
+                    .HasColumnName("productId");
+
+                entity.Property(e => e.quantity)
+                   .ValueGeneratedNever()
+                   .HasColumnName("quantity");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Code)
+                    .HasMaxLength(10)
+                    .HasColumnName("code")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Price)
+                    .HasColumnType("money")
+                    .HasColumnName("price");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .HasColumnName("email")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(100)
+                    .HasColumnName("lastname")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .IsFixedLength();
+
+                entity.Property(e => e.IsAdmin).HasColumnName("IsAdmin");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
